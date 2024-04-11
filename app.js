ec4c9134527d8c3fb5923641522f87d2126ea82e
nikolaytswetanow <85014024+nikolaytswetanow@users.noreply.github.com>
2024-04-11 22:21:26 +0300

    Update and rename scrollreveal.js to app.js

--- app.js
+++ app.js
@@ -30,3 +30,11 @@ if (history.scrollRestoration) {
     window.scrollTo(0, 0);
   };
 }
+
+var map = L.map("map").setView([43.26560675, 26.935116925801925], 13);
+var marker = L.marker([43.26560675, 26.935116925801925]).addTo(map);
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 18,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
